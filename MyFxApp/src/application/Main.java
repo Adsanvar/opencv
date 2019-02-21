ac26e14272575e61cca245bd22d087febaa8ae00
Adrian Sandoval <adriansandovalvargas@gmail.com>
2019-02-21 10:01:37 -0500

    Add files via upload

---
+++ MyFxApp/src/application/Main.java
@@ -0,0 +1,32 @@
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("FirstJFX.fxml"));
+			Scene scene = new Scene(root,800,600);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("Yelllow");
+			primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream( "AI.png" )));
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+
+
+	}
+}
